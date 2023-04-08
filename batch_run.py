a76d770609e93baec83e74426e5a09d7e1662213
Reza.Abbaszadeh <reza.abbaszadeh@partsoftware.com>
2023-04-09 03:41:42 +0430

    move command line arguments to batch run

--- batch_run.py
+++ batch_run.py
@@ -1,4 +1,10 @@
+# Run "python batch_run.py -h" for help
+# Sample command:
+# > python batch_run.py --path <PATH_TO_PNGs_DIR> -r 9 12 15 -d 3 6 9 -i 10 -s 1000
+#             *******all arguments are optional*******
+
 import os
+import argparse
 import pandas as pd
 from mesa.datacollection import DataCollector
 from cpp.batchrunner import batch_run_with_rngs
@@ -57,29 +63,60 @@ class BatchCoveragePathPlan(CoveragePathPlan):
             # agent_reporters={"first visits": lambda x: {'value': x.first_visits, 'color': x.color}},
             # agent_reporters={"first visits": lambda x: x.first_visits},
         )
+
+
+if __name__ == "__main__":
     
-maps = list(glob.glob(os.path.dirname(os.path.realpath(__file__)) + '\\cpp\\maps\\EX\\maps\\*.png'))
+    path = os.path.dirname(os.path.realpath(__file__))
+    robot_count = [9, 12, 15]
+    depths = [3,6,9]
+    iterations = 3
+    max_steps = 10000
 
-# parameter lists for each parameter to be tested in batch run
-br_params = {
-    "robot_count": [9, 12, 15],
-    "map": maps,
-    'depth': [1, 3, 5, 7, 9],
-}
-
-if __name__ == "__main__":  
-    print(len(maps),'maps')
-    # note that each parameter set is executed (2 * iterations) times. The second is obstacle free.
-    data = batch_run_with_rngs(
-        BatchCoveragePathPlan,
-        br_params,
-        max_steps=10000,
-        iterations=4,
-        display_progress=True
+    def dir_path(string):
+        if os.path.isdir(string):
+            return string
+        else:
+            raise NotADirectoryError(string)
+    
+    parser = argparse.ArgumentParser(
+        prog="Batch run",
+        description="This program executes batch run with the given argumens."
     )
+    parser.add_argument("--path", type=dir_path)
+    parser.add_argument("-r", "--Robots", nargs='+', type=int, help="List of number of robots.")
+    parser.add_argument("-d", "--Depths", nargs='+', type=int, help="List of depth of view of the robots.")
+    parser.add_argument("-i", "--Iterations", type=int, help="Number of interations each parameters set is executed.")
+    parser.add_argument("-s", "--Max_Steps", type=int, help="halts the simulation if steps passes this number")
+    args = parser.parse_args()
+
+    if args.Robots:
+        robot_count = args.Robots
+    if args.Depths:
+        depths = args.Depths
+    if args.Iterations:
+        iterations = args.Iterations
+    if args.Max_Steps:
+        max_steps = args.Max_Steps
+    if args.path:
+        path = args.path
+
+    maps = list(glob.glob(path + '\\*.png'))
+
+    br_params = {
+        "robot_count": robot_count,
+        "map": maps,
+        'depth': depths,
+    }
+    data = batch_run_with_rngs(
+            BatchCoveragePathPlan,
+            br_params,
+            max_steps=max_steps,
+            iterations=iterations,
+            display_progress=True
+        )
     br_df = pd.DataFrame(data)
     br_df = br_df.groupby(['robot_count','map','depth', 'iteration']).apply(merge) 
 
-    if not os.path.exists(os.path.dirname(os.path.realpath(__file__)) + "\\results"):
-        os.makedirs(os.path.dirname(os.path.realpath(__file__)) + '\\results')
-    br_df.to_csv(os.path.dirname(os.path.realpath(__file__)) + '\\results\\batch__dist.csv')
+    br_df.to_csv(path + '\\result.csv')
+    print('find results at: ', path + '\\result.csv')
