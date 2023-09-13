4231612727341ed5e29a34a6d98ab9f957628fa8
k1youssefi@edu.aau.at <SlayerTheGreat10003>
2023-09-13 12:04:57 +0200

    fix on loading map files

--- cpp/map.py
+++ cpp/map.py
@@ -6,27 +6,27 @@ from cpp.constants import AREA, OBS
 from cpp.shape import Shape
 
 def generate_map_from_png(path):
-    img = Image.open(path)
-    map = np.array(img)
-    non_obs = np.array(map).mean(axis=2) > 128
-    map = np.int8(np.zeros(non_obs.shape))
-    map[non_obs] = AREA
-    map[~non_obs] = OBS
-    map = np.flip(map, axis=0)
-    return map, img.width, img.height
+    img = Image.open(path).convert('L')
+    obstacle_map = np.array(img)
+    non_obs = obstacle_map > 128
+    obstacle_map = np.int8(np.zeros(non_obs.shape))
+    obstacle_map[non_obs] = AREA
+    obstacle_map[~non_obs] = OBS
+    obstacle_map = np.flip(obstacle_map, axis=0)
+    return obstacle_map, img.width, img.height
 
-def generate_map_by_pattern(pattern, map_shape, random): 
+def generate_map_by_pattern(pattern, obstacle_map_shape, random): 
     attemps = 0
     while True:
         try:
             attemps += 1
-            map = np.ones(map_shape)
+            obstacle_map = np.ones(obstacle_map_shape)
             obs = pattern[1:-1].split(',')
             shapes = [Shape.get_shape(re.sub(' +', ' ', ob).strip(), random) for ob in obs]
             for shape in shapes:
-                shape.draw(map)
-            return map
+                shape.draw(obstacle_map)
+            return obstacle_map
         except Exception as err:
-            print("error while generating map: {0}".format(err))
+            print("error while generating the obstacle_map: {0}".format(err))
             if attemps > 100:
                 raise Exception(err)
